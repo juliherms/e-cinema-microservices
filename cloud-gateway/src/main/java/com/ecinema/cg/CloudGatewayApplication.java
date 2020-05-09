f7802d82e64e8b588a7942dea3aec8a545e57336
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-09 17:08:03 -0300

    implement cloud gateway

---
+++ cloud-gateway/src/main/java/com/ecinema/cg/CloudGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.ecinema.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Microservice gateway application
+ * @author j.a.vasconcelos
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
---
+++ cloud-gateway/src/test/java/com/ecinema/cg/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecinema.cg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
