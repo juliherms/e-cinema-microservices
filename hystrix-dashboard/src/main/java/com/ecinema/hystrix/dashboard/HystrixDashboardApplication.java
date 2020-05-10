71d33495d7b5c1ac3095781a307c89f6aac72e19
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-10 06:43:40 -0300

    adding hystrix dashboard

---
+++ hystrix-dashboard/src/main/java/com/ecinema/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ecinema.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
