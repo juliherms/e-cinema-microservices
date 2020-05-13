b9f3311fd0bc29d357aa4e7a37846eb8d60a17de
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-13 12:47:21 -0300

    implement authorization service

---
+++ authorization-service/src/main/java/com/cinema/as/AuthorizationServiceApplication.java
@@ -0,0 +1,20 @@
+package com.cinema.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Microservice responsible to control access and authorization
+ * @author j.a.vasconcelos
+ *
+ */
+@EnableAuthorizationServer
+@SpringBootApplication
+public class AuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServiceApplication.class, args);
+	}
+
+}
